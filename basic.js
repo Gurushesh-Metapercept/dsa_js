fc86effeb1d7ee77453648f7813588e742aec369
Gurushesh-Metapercept <124129347+Gurushesh-Metapercept@users.noreply.github.com>
2025-10-08 13:30:31 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,36 @@
+// Find the Index of the givne num in a String
+// function helo(haystack, needle) {
+//   for (let i = 0; i < haystack.length; i++) {
+//     let found = true;
+//     for (let j = 0; j < needle.length; j++) {
+//       if (haystack[i + j] !== needle[j]) {
+//         found = false;
+//         break;
+//       }
+//     }
+//     if (found) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// let hh = helo(haystack, needle);
+// console.log(hh);
+
+// Count of Negative Numbers in an Array
+// function helo(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// let arr = [5, -2, 3, 4, -9, 6, -6, -3];
+
+// let hh = helo(arr);
+// console.log(hh);
