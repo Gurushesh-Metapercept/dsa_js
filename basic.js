c28efa7a082722e5c86a3ac10e91fff5b76f6ab5
Gurushesh-Metapercept <124129347+Gurushesh-Metapercept@users.noreply.github.com>
2025-10-16 11:27:20 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -90,3 +90,28 @@
 
 // let n = -565
 // console.log(countDigit(n))
+
+
+
+function PalindromNum(n) {
+    if (n < 0) return false
+
+    let xCopy = n
+    let rev = 0
+
+    while (n > 0) {
+        let rem = n % 10
+        rev = (10 * rev) + rem
+        n = Math.floor(n / 10)
+    }
+
+    if (xCopy == rev) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+let n = 1215
+console.log(PalindromNum(n))
